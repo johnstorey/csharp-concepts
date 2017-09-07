06d3da041ae48ad385ef6b0df2dab3d89623ad91
John R. Storey <john@binaryfeline.com>
2017-09-07 11:26:57 -0400

    Added CompareTo calls.
    - /culture-sensitive/, /case-sensitive/ order comparisons.
    - Default comparison.
    - Used sorted collections and similar.

--- StringComparisons.cs
+++ StringComparisons.cs
@@ -56,5 +56,33 @@ namespace Concept.UnitTests.String
     result = System.String.Compare("Atom", "Atom");
     Assert.True(result == 0, "Atom EQ Atom");
    }
+
+   // CompareTo implements IComparable and is *the standard
+   // comparison protocol*. This means it defines default
+   // ordering behavior of strings in applications such as
+   // sorted collections.
+   [Fact]
+   public void DefaultComparison()
+   {
+     // "atom" EQ "Atom"
+     var result = "atom".CompareTo("Atom");
+     Assert.False(result == 0, "atom EQ Atom");
+
+     // "atom" LT "Zambia" !
+     result = "atom".CompareTo("Zambia");
+      Assert.True(result < 0, "atom LT Zambia!");
+
+    // "atom" LT "zambia"
+    result = "atom".CompareTo("zambia");
+    Assert.True(result < 0, "atom LT zambia");
+
+    // "Atom" LT "zambia"
+    result = "Atom".CompareTo("Zambia");
+    Assert.True(result < 0, "Atom LT Zambia");
+
+    // "Atom" EQ "Atom"
+    result = "Atom".CompareTo("Atom");
+    Assert.True(result == 0, "Atom EQ Atom");
+   }
   }
 }
